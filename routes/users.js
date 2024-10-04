5e2c2249285252aadfd0718cfc4670944599360e
ddodoi <happycat129@naver.com>
2024-10-05 02:58:52 +0900

    second commit

--- routes/users.js
+++ routes/users.js
@@ -50,9 +50,16 @@ router.post('/login',
                     const token = jwt.sign({
                         email : loginUser.email,
                         name : loginUser.name
-                    }, process.env.PRIVATE_KEY)
+                    }, process.env.PRIVATE_KEY,{
+                        expiresIn:'30m',
+                        issuer : "jina"
+                    })
+
+                    console.log(token)
                     
-                    res.cookie("token", token)
+                    res.cookie("token", token, {
+                        httpOnly : true
+                    })
 
                     res.status(200).json({
                         message : `${loginUser.name}님 로그인 되었습니다.`,
